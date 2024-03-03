27a4e44f1e8aabe597635b535a94de9c83a73261
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-03-03 12:12:30 +0330

    Delete unused files and update manifest.json

--- background.js
+++ background.js
@@ -1,4 +1,12 @@
-chrome.scripting.executeScript({
-    target: {tabId: id, allFrames: true},
-    files: ['content_scripts/cscript.js'],
-});
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+    if (tab.url && tab.url.includes("youtube.com/watch")) {
+      const queryParameters = tab.url.split("?")[1];
+      const urlParameters = new URLSearchParams(queryParameters);
+  
+      chrome.tabs.sendMessage(tabId, {
+        type: "NEW",
+        videoId: urlParameters.get("v"),
+      });
+    }
+  });
+  
--- popup.js
+++ popup.js
@@ -1,14 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var fillButton = document.getElementById('fillButton');
-    fillButton.addEventListener('click', function() {
-      var messageSelect = document.getElementById('messageSelect');
-      var selectedMessage = messageSelect.options[messageSelect.selectedIndex].value;
-      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(
-          tabs[0].id,
-          {code: 'document.querySelector("div[contenteditable=true]").innerText = "' + selectedMessage + '";'}
-        );
-      });
-    });
-  });
-  
+// adding a new bookmark row to the popup
+const addNewBookmark = () => {};
+
+const viewBookmarks = () => {};
+
+const onPlay = e => {};
+
+const onDelete = e => {};
+
+const setBookmarkAttributes =  () => {};
+
+document.addEventListener("DOMContentLoaded", () => {});
