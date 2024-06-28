cd2b23de19c27e44aa6a01167d82bafe57b7f31e
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-06-28 08:30:25 +0430

    refactor: Improve message sending functionality and update CSS classes for message buttons

--- content.js
+++ content.js
@@ -1,50 +1,31 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "send_message") {
     var input = document.querySelector(
       'div[contenteditable="true"][data-tab="10"]'
     );
     if (input) {
-      // Focus on the input field
       input.focus();
-
-      // Clear existing text (optional)
       document.execCommand("selectAll", false, null);
       document.execCommand("delete", false, null);
-
-      // Insert the message
       document.execCommand("insertText", false, request.message);
-
-      // Trigger an input event to ensure WhatsApp recognizes the message
       input.dispatchEvent(
         new InputEvent("input", { bubbles: true, cancelable: true })
       );
 
-      // Send the message (click the send button)
       var sendButton = document.querySelector('button[data-tab="11"]');
       if (sendButton) {
         sendButton.click();
+        sendResponse({ status: "Message sent" });
       } else {
         console.error("Send button not found");
+        sendResponse({ status: "Send button not found" });
       }
     } else {
       console.error("Input field not found");
+      sendResponse({ status: "Input field not found" });
     }
+  } else if (request.action === "get_number") {
+    console.log("hello");
   }
-
-  if (request.action === "get_Number") {
-    const phonePattern = /\+93\s\d{2}\s\d{3}\s\d{4}/g;
-
-    // Get the text content of the body
-    const bodyText = document.body.textContent;
-
-    // Find all matches in the text content
-    const matches = bodyText.match(phonePattern);
-
-    // Output the matches
-    if (matches) {
-      console.log("Found phone numbers:", matches);
-    } else {
-      console.log("No phone numbers found.");
-    }
-  }
+  return true; // Ensure the sendResponse can be used asynchronously
 });
