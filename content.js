07c7464bea11fbe5ff5b2a14f9aa8fdfe16e0878
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2024-07-02 12:23:20 +0430

    refactor: Improve phone number retrieval and update message sending functionality

--- content.js
+++ content.js
@@ -1,5 +1,3 @@
-// content.js
-
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === "send_message") {
     sendMessage(request.message, sendResponse);
@@ -40,10 +38,8 @@ function sendMessage(message, sendResponse) {
 }
 
 function handleGetNumber(sendResponse) {
-  // Get all div elements with role="button" inside the header
   const buttonDivs = document.querySelector("div#main");
   const header = buttonDivs.querySelector("header");
-  // Select the second div inside the header
   const secondDiv = header.querySelectorAll("div")[1];
   if (secondDiv) {
     secondDiv.click();
@@ -54,6 +50,7 @@ function handleGetNumber(sendResponse) {
           `http://192.168.9.14/YadakShop-APP/views/callcenter/main.php?phone=${phoneNumber}`,
           "_blank"
         );
+        sendResponse({ status: "Phone number opened" });
       }
     }, 1000);
   } else {
@@ -63,10 +60,8 @@ function handleGetNumber(sendResponse) {
 }
 
 function handleCreateBill(sendResponse) {
-  // Get all div elements with role="button" inside the header
   const buttonDivs = document.querySelector("div#main");
   const header = buttonDivs.querySelector("header");
-  // Select the second div inside the header
   const secondDiv = header.querySelectorAll("div")[1];
   if (secondDiv) {
     secondDiv.click();
@@ -77,6 +72,7 @@ function handleCreateBill(sendResponse) {
           `http://192.168.9.14/YadakShop-APP/views/factor/createIncomplete.php?phone=${phoneNumber}`,
           "_blank"
         );
+        sendResponse({ status: "Bill creation opened" });
       }
     }, 1000);
   } else {
@@ -86,63 +82,45 @@ function handleCreateBill(sendResponse) {
 }
 
 function getInfoNumber() {
-  // Select the header element with tabindex="-1"
   const header = document.querySelector('div[tabindex="-1"]');
-
   if (header) {
     const directChildDivs = header.querySelectorAll("div");
-
-    // Filter out direct child div elements
     const directChildDivsArray = Array.from(directChildDivs).filter(
       (div) => div.parentElement === header
     );
-
-    // Select the 5th direct child div (index 4)
     const fifthDiv = directChildDivsArray[4];
-
     if (fifthDiv) {
       let section = fifthDiv.querySelector(
         "span > div > span > div > div > section"
       );
       const directChild = section.querySelectorAll("div");
-
-      // Filter out direct child div elements
       const directChildDivsArray = Array.from(directChild).filter(
         (div) => div.parentElement === section
       );
       try {
-        // Select the 5th direct child div (index 4)
         const firstDiv = directChildDivsArray[0];
         const directChildren = firstDiv.querySelectorAll("div");
-
-        // Filter out direct child div elements
         const ChildDivsArray = Array.from(directChildren).filter(
           (div) => div.parentElement === firstDiv
         );
-
         const secondDiv = directChildDivsArray[0];
         const phoneNumber =
           secondDiv.children[1].children[1].querySelector(
             "div span span span"
           ).innerText;
-
         return modifyPhoneNumber(phoneNumber);
       } catch (error) {
         try {
-          // Select the 5th direct child div (index 4)
           const firstDiv = directChildDivsArray[6];
           const phoneContainer = firstDiv.children[2];
           const phoneNumber =
             phoneContainer.querySelector("div div span span").innerText;
-
           return modifyPhoneNumber(phoneNumber);
         } catch (error) {
-          // Select the 5th direct child div (index 4)
           const firstDiv = directChildDivsArray[7];
           const phoneContainer = firstDiv.children[2];
           const phoneNumber =
             phoneContainer.querySelector("div div span span").innerText;
-
           return modifyPhoneNumber(phoneNumber);
         }
       }
@@ -168,58 +146,6 @@ function getChatList() {
   const chatList = document.querySelectorAll('div[role="listitem"]');
   return chatList;
 }
-function appendButton(header) {
-  const container = document.createElement("div");
-  container.id = "customContainer";
-  container.style.marginLeft = "10px";
-
-  const content = document.createElement("div");
-  content.className = "btn btn-primary";
-  content.innerHTML = `<div>
-                        <section>
-                          <h2>پیام مدنظر خود را برای ارسال انتخاب کنید</h2>
-                          <button class="btn msgBtn" type="button" data-text="سلام" id="message1">سلام</button>
-                          <button class="btn msgBtn" type="button" data-text="حال شما چطور است؟" id="message2">حال شما چطور است؟</button>
-                          <button class="btn msgBtn" type="button" data-text="خدانگهدار" id="message3">خدانگهدار</button>
-                        </section>
-                        <section id="operations">
-                          <button class="btn btnSubmit" type="button" id="cartable">کارتابل</button>
-                          <button class="btn btnSubmit" type="button" id="factor">فاکتور</button>
-                        </section>
-                      </div>`;
-
-  const additionalContent = document.createElement("div");
-  additionalContent.innerHTML = `
-    `;
-  header.appendChild(content);
-
-  // Attach event listeners to the buttons
-  document
-    .getElementById("message1")
-    .addEventListener("click", () =>
-      sendMessage("سلام", (response) => console.log(response))
-    );
-  document
-    .getElementById("message2")
-    .addEventListener("click", () =>
-      sendMessage("حال شما چطور است؟", (response) => console.log(response))
-    );
-  document
-    .getElementById("message3")
-    .addEventListener("click", () =>
-      sendMessage("خدانگهدار", (response) => console.log(response))
-    );
-  document
-    .getElementById("cartable")
-    .addEventListener("click", () =>
-      handleGetNumber((response) => console.log(response))
-    );
-  document
-    .getElementById("factor")
-    .addEventListener("click", () =>
-      handleCreateBill((response) => console.log(response))
-    );
-}
 
 setTimeout(() => {
   let chatList = getChatList();
@@ -228,7 +154,87 @@ setTimeout(() => {
   }
   chatList.forEach((chat) =>
     chat.addEventListener("click", () => {
-      appendButton(document.getElementById("main"));
+      setTimeout(() => {
+        const main = document.getElementById("main");
+        appendButton(main);
+      }, 500);
     })
   );
 }, 5000);
+
+function appendButton(header) {
+  const container = document.createElement("div");
+  container.id = "customContainer";
+  container.style.marginLeft = "10px";
+
+  const content = document.createElement("div");
+  content.innerHTML = `<div style="background-color:white; direction:rtl !important;">
+                        <section>
+                          <button class="customBtn msgBtn" type="button" data-text="سلام" id="message1">سلام</button>
+                          <button class="customBtn msgBtn" type="button" data-text="حال شما چطور است؟" id="message2">حال شما چطور است؟</button>
+                          <button class="customBtn msgBtn" type="button" data-text="خدانگهدار" id="message3">خدانگهدار</button>
+                        </section>
+                        <section id="operations">
+                          <button class="customBtn btnSubmit" type="button" id="cartable">کارتابل</button>
+                          <button class="customBtn btnSubmit" type="button" id="factor">فاکتور</button>
+                        </section>
+                      </div>`;
+  const style = document.createElement("style");
+  style.innerHTML = `
+          .customBtn {
+              border: none;
+              color: white;
+              padding: 5px 10px;
+              border-radius: 5px;
+              text-align: center;
+              text-decoration: none;
+              display: inline-block;
+              font-size: 12px;
+              margin: 4px 2px;
+              cursor: pointer !important;
+          }
+
+          .msgBtn {
+              background-color: #3a6dcd;
+          }
+
+          .btnSubmit {
+              display: inline-flex;
+              align-items: center;
+              background-color: seagreen;
+              cursor: pointer !important;
+          }`;
+  document.head.appendChild(style);
+
+  header.insertAdjacentHTML(
+    "beforeend",
+    `<div style="background-color:white; direction:rtl !important; z-index:100000000000000 !important">
+                        <section>
+                          <button class="customBtn msgBtn" type="button" data-text="سلام" id="message1">سلام</button>
+                          <button class="customBtn msgBtn" type="button" data-text="حال شما چطور است؟" id="message2">حال شما چطور است؟</button>
+                          <button class="customBtn msgBtn" type="button" data-text="خدانگهدار" id="message3">خدانگهدار</button>
+                        </section>
+                        <section id="operations">
+                          <button class="customBtn btnSubmit" type="button" id="cartable">کارتابل</button>
+                          <button class="customBtn btnSubmit" type="button" id="factor">فاکتور</button>
+                        </section>
+                      </div>`
+  );
+
+  // Attach event listeners to the buttons
+  document.getElementById("message1").addEventListener("click", () => {
+    sendMessage("سلام", (response) => console.log(response));
+  });
+  document.getElementById("message2").addEventListener("click", () => {
+    sendMessage("حال شما چطور است؟", (response) => console.log(response));
+  });
+  document.getElementById("message3").addEventListener("click", () => {
+    sendMessage("خدانگهدار", (response) => console.log(response));
+  });
+  document.getElementById("cartable").addEventListener("click", () => {
+    handleGetNumber((response) => console.log(response));
+  });
+  document.getElementById("factor").addEventListener("click", () => {
+    handleCreateBill((response) => console.log(response));
+  });
+}
